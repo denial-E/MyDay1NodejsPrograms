466087475b20dfd57ed79be378c23da03c600916
denial-E <shempeter221992@gmail.com>
2024-11-29 02:52:30 +0530

    run bro

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import fs from 'fs';
+import {format} from 'date-fns';
 
 const app= express();
 const PORT=4000;
@@ -9,7 +11,14 @@ app.get('/', (req,res)=>{
 app.get('/get-data', (req,res)=>{
     res.status(200).json({messege:"data",data:{name:"god"}})
 })
-
+app.get('/write-read', (req,res)=>{
+    let today= format(new Date(), 'dd-mm-yyyy-hh-mm-ss')
+    console.log(("today",today));
+    const filePath= `TimeStamp/${today}.txt`
+    fs.writeFileSync(filePath,`${today}`,'utf8')
+    let data= fs.readFileSync(filePath,'utf8')
+    res.status(200).send(data)
+})
 app.listen(PORT, ()=>{
   console.log(`App is running in the port = ${PORT}`);
 })//running
